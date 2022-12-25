31a9ee706e61edfc99ef60e6b2cfb32d67a4037b
adarshajit <adarshajit@gmail.com>
2022-12-25 20:20:04 +0530

    feat: added the ability to customise hair style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-import './App.css'
+import React, { useState } from 'react';
+import { HAIR_STYLES } from './constants';
+import Avatar from './Avatar';
+import './App.css';
 
-const App = () => (
-    <div className="App">
-      <h1>People DB</h1>
-    </div>
-  );
+const App = () => {
+   const [hairStyle, setHairStyle] = useState('noHair1');
+
+   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+      setHairStyle(e.target.value);
+   };
+
+   return (
+      <div>
+         <h1>People DB</h1>
+         <select placeholder="Choose hair style" onChange={handleChange}>
+            <option value="noHair1">-- choose a hair style --</option>
+            {HAIR_STYLES.map((hair, idx) => (
+               <option value={hair} key={idx + 1}>
+                  {hair}
+               </option>
+            ))}
+         </select>
+         <Avatar hairStyle={hairStyle} />
+      </div>
+   );
+};
 
 export default App;
