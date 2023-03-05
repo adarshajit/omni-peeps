836bb57280cad92818f3a7a2898ec3e125cc175f
adarshajit <adarshajit@gmail.com>
2023-03-05 16:50:36 +0530

    chore: moved initial values of avatarStyle to constants folder

---
+++ src/constants/avatarStyle.ts
@@ -0,0 +1,10 @@
+export const INITIAL_VALUES = {
+   head: 'noHair1',
+   face: 'smile',
+   facialHair: '',
+   accessories: '',
+   skinColor: 'd08b5b',
+   clothingColor: 'fdea6b',
+   facialHairProbability: 0,
+   accessoriesProbability: 0,
+};
